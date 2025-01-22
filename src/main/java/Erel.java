88fc386c86d821b1d27db5cf26be93463558a0eb
JohannsenLum <lumjohannsen@gmil.com>
2025-01-22 23:22:06 +0800

    Added Enums

--- src/main/java/Erel.java
+++ src/main/java/Erel.java
@@ -2,6 +2,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Erel {
+    public enum Command {
+        BYE, LIST, MARK, UNMARK, DELETE, TODO, DEADLINE, EVENT;
+
+        public static Command fromString(String input) throws IllegalArgumentException {
+            return Command.valueOf(input.toUpperCase());
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String lines = "___________________________________________";
@@ -12,17 +20,22 @@ public class Erel {
         System.out.println(spaces+ lines + "\n " + spaces + "Hello! I'm Erel\n " + spaces + "What can I do for you?\n" + spaces + lines + "\n");
 
 
-        label:
+
         while(true) {
             try {
                 String input = sc.nextLine();
                 String action = input.split(" ")[0];
+                String[] inputArr = input.split(" /");
 
+                // Parse action into enum Commands
+                Command command = Command.fromString(action);
 
-                switch (action) {
-                    case "bye":
-                        break label;
-                    case "list":
+                switch (command) {
+                    case BYE: {
+                        System.out.println(spaces + lines + "\n " + spaces + "Bye. Hope to see you again soon!\n" + spaces + lines);
+                        return;
+                    }
+                    case LIST: {
                         System.out.println(spaces + lines);
                         System.out.println(spaces + " Here are the tasks in your list:");
                         int counter = 1;
@@ -31,7 +44,8 @@ public class Erel {
                         }
                         System.out.println(spaces + lines + "\n");
                         break;
-                    case "mark": {
+                    }
+                    case MARK: {
                         int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
                         arrList.get(taskNumber).setDone(true);
                         System.out.println(spaces + lines + "\n " + spaces + "Nice! I've marked this task as done:");
@@ -40,7 +54,7 @@ public class Erel {
 
                         break;
                     }
-                    case "unmark": {
+                    case UNMARK: {
                         int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
                         arrList.get(taskNumber).setDone(false);
                         System.out.println(spaces + lines + "\n " + spaces + "Ok, I've marked this task as not done yet:");
@@ -49,7 +63,7 @@ public class Erel {
 
                         break;
                     }
-                    case "delete": {
+                    case DELETE: {
                         int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
                         Task t = arrList.get(taskNumber);
                         arrList.remove(taskNumber);
@@ -59,49 +73,54 @@ public class Erel {
 
                         break;
                     }
-                    default:
-                        // Adding the data from user into arrList of Tasks
-                        String[] inputArr = input.split(" /");
-                        switch (action) {
-                            case "todo" -> {
-                                if (input.length() <= 5) {
-                                    throw new ErelException("OOPS!!! The description of a todo cannot be empty.");
-                                }
-
-                                arrList.add(new Todo(inputArr[0].substring(5)));
-                            }
-                            case "deadline" -> {
-                                if (inputArr.length < 2 || inputArr[1].length() <= 3) {
-                                    throw new ErelException("OOPS!!! A deadline must include '/by' followed by a time.");
-                                }
-
-                                arrList.add(new Deadline(inputArr[0].substring(9), inputArr[1].substring(3)));
-                            }
-                            case "event" -> {
-                                if (inputArr.length < 3 || inputArr[1].length() <= 5 || inputArr[2].length() <= 3) {
-                                    throw new ErelException("OOPS!!! An event must include '/from' and '/to'.");
-                                }
-
-                                arrList.add(new Event(inputArr[0].substring(6), inputArr[1].substring(5), inputArr[2].substring(3)));
-                            }
-                            default -> throw new Exception(" OOPS!!! Something went wrong. Please try again.");
+                    case TODO: {
+                        if (input.length() <= 5) {
+                            throw new ErelException("OOPS!!! The description of a todo cannot be empty.");
                         }
 
+                        arrList.add(new Todo(inputArr[0].substring(5)));
                         System.out.println(spaces + lines + "\n " + spaces + "Got it. I've added this task:");
                         System.out.println(spaces + spaces + arrList.get(arrList.size() - 1));
                         System.out.println(spaces + " Now you have " + arrList.size() + " tasks in the list." + "\n" + spaces + lines + "\n");
                         break;
-                }
+                    }
+                    case DEADLINE: {
+                        if (inputArr.length < 2 || inputArr[1].length() <= 3) {
+                            throw new ErelException("OOPS!!! A deadline must include '/by' followed by a time.");
+                        }
+
+                        arrList.add(new Deadline(inputArr[0].substring(9), inputArr[1].substring(3)));
+                        System.out.println(spaces + lines + "\n " + spaces + "Got it. I've added this task:");
+                        System.out.println(spaces + spaces + arrList.get(arrList.size() - 1));
+                        System.out.println(spaces + " Now you have " + arrList.size() + " tasks in the list." + "\n" + spaces + lines + "\n");
+                        break;
+                    }
+                    case EVENT: {
+                        if (inputArr.length < 3 || inputArr[1].length() <= 5 || inputArr[2].length() <= 3) {
+                            throw new ErelException("OOPS!!! An event must include '/from' and '/to'.");
+                        }
+
+                        arrList.add(new Event(inputArr[0].substring(6), inputArr[1].substring(5), inputArr[2].substring(3)));
+                        System.out.println(spaces + lines + "\n " + spaces + "Got it. I've added this task:");
+                        System.out.println(spaces + spaces + arrList.get(arrList.size() - 1));
+                        System.out.println(spaces + " Now you have " + arrList.size() + " tasks in the list." + "\n" + spaces + lines + "\n");
+                        break;
+                    }
+                    default: throw new Exception(" OOPS!!! Something went wrong. Please try again.");
+                    }
+
             } catch (ErelException e) {
                 System.out.println(spaces + lines + "\n " + spaces + e.getMessage());
                 System.out.println(spaces + lines + "\n");
+            } catch (IllegalArgumentException e) {
+                System.out.println(spaces + lines + "\n " + spaces + "Invalid command. Please try again.");
+                System.out.println(spaces + lines + "\n");
             } catch (Exception e) {
-                System.out.println(spaces + lines + "\n " + spaces + e.getMessage());
+                System.out.println(spaces + lines + "\n " + spaces + "An error occurred: " + e.getMessage());
                 System.out.println(spaces + lines + "\n");
             }
         }
 
-        System.out.println(spaces + lines + "\n " + spaces + "Bye. Hope to see you again soon!\n" + spaces + lines);
     }
 }
 
