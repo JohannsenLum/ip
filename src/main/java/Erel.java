2be020c49b1c24965d03b11410756c8397fdb7bc
JohannsenLum <lumjohannsen@gmil.com>
2025-01-22 19:34:16 +0800

    Added add, list

--- src/main/java/Erel.java
+++ src/main/java/Erel.java
@@ -1,18 +1,34 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Erel {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String lines = "________________________________";
         String spaces = "    ";
-        System.out.println(lines + "\nHello! I'm Erel\nWhat can I do for you?\n" + lines + "\n");
+        int counter = 1;
+        ArrayList<String> arrList = new ArrayList<>();
 
-        Scanner sc = new Scanner(System.in);
+        System.out.println(spaces+ lines + "\n " + spaces + "Hello! I'm Erel\n " + spaces + "What can I do for you?\n" + spaces + lines + "\n");
+
+
         while(true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {break;}
-            System.out.println(lines + "\n" + input + "\n" + lines + "\n");
+            if (input.equals("list")) {
+                System.out.println(spaces + lines);
+                for (String s : arrList) {
+                    System.out.println(spaces + " " + counter++ + ". " + s);
+                }
+                System.out.println(spaces + lines);
+            }else {
+                arrList.add(input);
+                System.out.println(spaces + lines + "\n " + spaces + "added: " + input + "\n" + spaces + lines + "\n");
+            }
+
         }
 
-        System.out.println(lines + "\nBye. Hope to see you again soon!\n" + lines + "\n");
+        System.out.println(spaces + lines + "\n " + spaces + "Bye. Hope to see you again soon!\n" + spaces + lines);
     }
 }
+
