34d6701b43e6077239656f2b5577ec8dc4d57d05
JohannsenLum <lumjohannsen@gmil.com>
2025-01-22 19:30:34 +0800

    Updated echo with correct spaces

--- src/main/java/Erel.java
+++ src/main/java/Erel.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Erel {
     public static void main(String[] args) {
         String lines = "________________________________";
-
-        System.out.println(lines + "\nHello! I'm Erel\nWhat can I do for you?\n" + lines);
+        String spaces = "    ";
+        System.out.println(lines + "\nHello! I'm Erel\nWhat can I do for you?\n" + lines + "\n");
 
         Scanner sc = new Scanner(System.in);
         while(true) {
             String input = sc.nextLine();
             if (input.equals("bye")) {break;}
-            System.out.println(lines + "\n" + input + "\n" + lines);
+            System.out.println(lines + "\n" + input + "\n" + lines + "\n");
         }
 
-        System.out.println(lines + "\nBye. Hope to see you again soon!\n" + lines);
+        System.out.println(lines + "\nBye. Hope to see you again soon!\n" + lines + "\n");
     }
 }
