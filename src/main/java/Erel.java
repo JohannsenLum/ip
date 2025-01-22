1281a542040d19928d981cd35ff9dcb437e73cd6
JohannsenLum <lumjohannsen@gmil.com>
2025-01-22 15:04:38 +0800

    Echoing User's commands

--- src/main/java/Erel.java
+++ src/main/java/Erel.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Erel {
     public static void main(String[] args) {
         String lines = "________________________________";
-        System.out.println(lines);
-        System.out.println("Hello! I'm Erel\nWhat can I do for you?");
-        System.out.println(lines);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(lines);
+
+        System.out.println(lines + "\nHello! I'm Erel\nWhat can I do for you?\n" + lines);
+
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {break;}
+            System.out.println(lines + "\n" + input + "\n" + lines);
+        }
+
+        System.out.println(lines + "\nBye. Hope to see you again soon!\n" + lines);
     }
 }
