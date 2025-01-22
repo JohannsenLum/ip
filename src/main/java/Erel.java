03914212a3c9c9646fc91a402b21880e995b58e4
JohannsenLum <lumjohannsen@gmil.com>
2025-01-22 14:53:05 +0800

    Renaming, added greetings, exit

---
+++ src/main/java/Erel.java
@@ -0,0 +1,10 @@
+public class Erel {
+    public static void main(String[] args) {
+        String lines = "________________________________";
+        System.out.println(lines);
+        System.out.println("Hello! I'm Erel\nWhat can I do for you?");
+        System.out.println(lines);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(lines);
+    }
+}
