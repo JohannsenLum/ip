9b8c9d79f28270fe97d5004c0dd5bcb44e4c86eb
JohannsenLum <lumjohannsen@gmil.com>
2025-02-15 20:49:48 +0800

    Update avatars for Erel Bot and user
    Erel Bot's avatar has been replaced with a friendly, futuristic chatbot
    mascot to enhance its visual appeal and reflect its role as an AI
    assistant. The user avatar has been updated to a modern gamer-themed
    character to better align with the desired identity.

--- src/main/java/erel/gui/MainWindow.java
+++ src/main/java/erel/gui/MainWindow.java
@@ -23,8 +23,8 @@ public class MainWindow extends AnchorPane {
 
     private Erel erel;
 
-    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
-    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/User.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/Erel.png"));
 
     @FXML
     public void initialize() {
