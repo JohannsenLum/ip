68e558bce8882c48ec0b991505a38f3184cc43b2
JohannsenLum <lumjohannsen@gmil.com>
2025-02-15 21:19:52 +0800

    Add title to main UI

--- src/main/java/erel/gui/Main.java
+++ src/main/java/erel/gui/Main.java
@@ -29,6 +29,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Erel");
             fxmlLoader.<MainWindow>getController().setErel(erel); // inject the Erel instance
             stage.show();
         } catch (IOException e) {
